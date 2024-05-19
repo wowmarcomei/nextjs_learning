a8f36d60b7c030f02834f4c150c0a572f6832982
laomei <wowmarcomei@163.com>
2024-05-19 17:01:09 +0800

    update page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,18 +1,8 @@
-// import Image from "next/image";
-
-// export default function Page() {
-//   return (
-//     <h1>hello, Next.js!</h1>
-//   );
-// }
-
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <div>
-      <h1>hello, Next.js!</h1>
-     <Link href="/dashboard">Dashboard</Link>
-    </div>
+    <h1>hello, Next.js!</h1>
   );
 }
+
